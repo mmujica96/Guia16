2ba937a816c24fbf38a38078e2846869d300ba81
mmujica96 <mmujica@espol.edu.ec>
2022-08-01 11:27:38 -0500

    terminando pracica en clase

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+router.get('/fff', function(req, res, next) {
+  res.send('respond with a resource in fff');
+});
 module.exports = router;
